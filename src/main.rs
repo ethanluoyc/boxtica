b061cb0df55454d67c1de84c78db0f858ea81e90
Fangyi Zhou <fangyi.zhou@yuriko.moe>
2018-01-28 10:33:17 +0000

    Make code compile

--- src/main.rs
+++ src/main.rs
@@ -38,7 +38,7 @@ pub fn main() {
                 },
                 _ => {}
             }
-            renderer.draw(&Block::Boxtica);
+            renderer.draw();
         }
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
