739f98d9be25bf666b031e6ff41fb197777a4d9c
Fangyi Zhou <fangyi.zhou@yuriko.moe>
2018-01-28 00:45:31 +0000

    Add next step for Direction

--- src/push_box/Direction.rs
+++ src/push_box/Direction.rs
@@ -4,3 +4,14 @@ pub enum Direction {
     Left,
     Right,
 }
+
+impl Direction {
+    pub fn next_step(&self, x: i32, y: i32) -> (i32, i32) {
+        match *self {
+            Direction::Up => (x, y+1),
+            Direction::Down => (x, y-1),
+            Direction::Left => (x-1, y),
+            Direction::Right => (x+1, y),
+        }
+    }
+}
