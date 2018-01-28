00a43960b1db58c9ee0741e785ff4154142c7dd4
Fangyi Zhou <fangyi.zhou@yuriko.moe>
2018-01-28 00:51:17 +0000

    Add helper methods for Block

--- src/push_box/Block.rs
+++ src/push_box/Block.rs
@@ -6,3 +6,19 @@ pub enum Block {
     // Whether target has a box or not
     Target(bool),
 }
+
+impl Block {
+    pub fn can_place_item(&self) -> bool {
+        match *self {
+            Block::Empty | Block::Target(false) => true,
+            _ => false,
+        }
+    }
+
+    pub fn can_move(&self) -> bool {
+        match *self {
+            Block::Boxtica | Block::Target(true) => true,
+            _ => false,
+        }
+    }
+}
